35ac179f51b289228a4f806e0828ca963ae14b56
lawiparadise <lawiparadise@gmail.com>
2017-03-11 04:22:30 +0900

    holepunching..

--- api_hole.py
+++ api_hole.py
@@ -9,10 +9,20 @@ svrsock = socket(AF_INET, SOCK_DGRAM)
 svrsock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 svrsock.bind((UDP_IP, UDP_PORT))
 
+# rasp IP PORT
+RASP = None
+RASP_IP = None
+RASP_PORT = None
+
 # receive and sendto
 while True:
     data, addr = svrsock.recvfrom(1024)
     print("received msg : ", data, " from ", addr)
 
     print('addr[0]', addr[0])
-    svrsock.sendto('im server'.encode('utf-8'), addr)
+    if addr[0] == '52.79.188.83' and not RASP:
+        # svrsock.sendto('holl'.encode('utf-8'), (RASP_IP, RASP_PORT))
+        svrsock.sendto('holl'.encode('utf-8'), RASP)
+    else:
+        RASP = addr
+        svrsock.sendto('im server, nice to meet you rasp!!'.encode('utf-8'), addr)
