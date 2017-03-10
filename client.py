367ee41a262b3ad5c78e1a4225238408786e8079
lawiparadise <lawiparadise@gmail.com>
2017-03-11 03:24:49 +0900

    fix sock

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from socket import *
 csock = socket(AF_INET, SOCK_DGRAM)
 csock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 csock.bind(('', 3334))
-csock.sendto('im client'.encode('utf-8'), ('172.16.101.61', 34624))
+csock.sendto('im client'.encode('utf-8'), ('13.124.19.161', 5001))
 s, addr = csock.recvfrom(1024)
 print('s', s)
 print('addr', addr)
