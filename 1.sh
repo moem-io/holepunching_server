2762a39da606893833ed4878d360deb06add3863
lawiparadise <lawiparadise@gmail.com>
2017-04-07 08:35:41 +0000

    fixing

---
+++ 1.sh
@@ -0,0 +1,5 @@
+while true; do
+date +"%T"
+python 2_to_rasp_by_another.py
+sleep 100
+done
---
+++ 3_to_no_hole.py
@@ -0,0 +1,19 @@
+from socket import *
+
+#
+UDP_IP = '61.43.139.24'
+UDP_PORT = 3334
+MESSAGE = "get,info"
+
+print("UDP target IP : ", UDP_IP)
+print("UDP target port : ", UDP_PORT)
+print("MESSAGE : ", MESSAGE)
+
+# init port:3334
+csock = socket(AF_INET, SOCK_DGRAM)
+csock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+csock.bind(('', 5001))
+
+# sendto MSG, IP, PORT
+csock.sendto(MESSAGE.encode('utf-8'), (UDP_IP, UDP_PORT))
+print("send")
