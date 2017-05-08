042e57fc5293527d4928c212fbb182fe0bb711d1
lawiparadise <lawiparadise@gmail.com>
2017-05-09 00:25:05 +0900

    mqtt and rabbit

--- motor_pre.py
+++ motor_pre.py
@@ -1,5 +1,19 @@
 # motor_pre
 import threading
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
+
+
 motor_end = False
 def get_motor(second=10):
 
--- sub_mq.py
+++ sub_mq.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import pika
 
 def on_connect(client, userdata, rc):
     print('connected with result'+str(rc))
@@ -6,6 +7,17 @@ def on_connect(client, userdata, rc):
 
 def on_message(client, userdata, msg):
     print("Topic: ", msg.topic + '\nMessage: ' + str(msg.payload))
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
 
 
 client = mqtt.Client()
