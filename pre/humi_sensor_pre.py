14e88bdc1d28ea47531221b169ee5501d6c57799
lawiparadise <lawiparadise@gmail.com>
2017-06-14 22:28:06 +0900

    fix temp, humi sensor

--- pre/humi_sensor_pre.py
+++ pre/humi_sensor_pre.py
@@ -2,20 +2,39 @@
 from requests import get
 import json
 import time
+from app import session
+from app.models.app_setting import AppSetting
+from app.models.app_log import AppLog
+import datetime
+from requests import post
+from manager.make_app import AlchemyEncoder
+import pika
+
 
 sensor_first = True
 
 def humidityFromSensor():
     global sensor_first
-    temp = 0
+    global rabbit_app_id
+    global input_sw
+    global input_val
     if sensor_first:
         sensor_first = False
     else:
         time.sleep(10)
 
-    # todo
-    node_id = 1
-    seoor_id = 1
-    humi = 24
-    print('humi sensor: ', humi)
-    return humi
+    sett = session.query(AppSetting).filter_by(app_id=rabbit_app_id).first()
+
+    # rabbit
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='sensor_q')
+    channel.basic_publish(exchange='',
+                          routing_key='sensor_q',
+                          body=str(sett.in_node) + ',' + str(sett.in_sensor) + ',' + str(rabbit_app_id) + ',' + 'humi')
+    connection.close()
+
+    while input_sw:
+        continue
+    input_sw = True
+    return input_val
